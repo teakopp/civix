47c0618b51e6268a4f3d2c50a83f8f759100936a
teakopp <teakopp@protonmail.com>
2023-02-11 14:20:13 -0800

    create navbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,1 +1,28 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+function Home() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          Civix
+          <Button color="inherit">Admin</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Home;
